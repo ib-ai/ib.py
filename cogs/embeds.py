1f5fadec78c8361f39ae5614c461350051f06adf
veeru153 <32760644+veeru153@users.noreply.github.com>
2022-12-09 22:52:38 +0530

    implemented embedraw

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -1,24 +1,33 @@
 import discord
 from discord.ext import commands
+import json
 
 
 class Embeds(commands.Cog):
+
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-    
+
     @commands.hybrid_command()
     async def embed(self, ctx: commands.Context):
         """
         Interactively construct a Discord embed.
         """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-    
+        raise NotImplementedError(
+            'Command requires implementation and permission set-up.')
+
     @commands.command()
-    async def embedraw(self, ctx: commands.Context):
+    async def embedraw(self, ctx: commands.Context, *, embedData):
         """
         Create a Discord embed via raw JSON input.
         """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
+        try:
+            embedBody = json.loads(embedData)
+            embed = discord.Embed.from_dict(embedBody)
+            await ctx.send(embed=embed)
+        except Exception as ex:
+            print(ex)
+            await ctx.send("An Error Occurred")
 
 
 async def setup(bot: commands.Bot):
