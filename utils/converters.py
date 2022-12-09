0900b7462b3b9895274c4bbcd2373597165d31f3
ilikecubesnstuff <subho1618@gmail.com>
2022-12-09 11:15:00 +0000

    `updates` code review fixes. Use bad-path-first in Index converter. Fix bug for ordinals of 11, 12 and 13.

--- utils/converters.py
+++ utils/converters.py
@@ -3,6 +3,6 @@ from discord.ext import commands
 
 def Index(arg: str):
     n = int(arg)
-    if n > 0:
-        return n
-    raise commands.BadArgument('Index must be a positive integer.')
+    if n <= 0:
+        raise commands.BadArgument('Index must be a positive integer.')
+    return n
--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,10 @@
 def ordinal(n: int) -> str:
+    if n < 0:
+        raise ValueError('Negative ordinal - ordinal must be passed a non-negative integer.')
+
+    match n//10 % 10:  # for numbers trailing with 11, 12, or 13
+        case 1:
+            return f'{n}th'
     match n%10:
         case 1:
             return f'{n}st'
