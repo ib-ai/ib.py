96758335e8972cd360b360a25a1b7e37d9b137f2
NathanealV <18462634+NathanealV@users.noreply.github.com>
2022-12-11 23:38:42 +1030

    Added docstring and logging as exception

--- utils/commands.py
+++ utils/commands.py
@@ -2,12 +2,15 @@ import logging
 from discord.ext import commands
 
 async def available_subcommands(ctx: commands.Context):
+    """
+    Send a list of available subcommands in a group command.
+    """
     subcmds = []
     for cmd in ctx.command.commands:
         try:
             usable = await cmd.can_run(ctx)
             if usable:
                 subcmds.append('`'+cmd.name+'`')
-        except commands.CommandError as e:
-            logging.debug(f'Command "{cmd.name}" check threw error, discarded in {ctx.command.name} group subcommand list.')
+        except commands.CommandError:
+            logging.exception(f'Command "{cmd.name}" check threw error, discarded in {ctx.command.name} group subcommand list.')
     await ctx.send(f'Available subcommands: {", ".join(subcmds)}.')
