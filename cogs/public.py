d6910e3ceafab7061598a377058446af3fd63d65
Max Gurbanli <gurbanlimaksud@gmail.com>
2022-12-28 22:34:23 +0100

    Add ping and userinfo

--- cogs/public.py
+++ cogs/public.py
@@ -15,6 +15,7 @@ class Public(commands.Cog):
         embed = discord.Embed(color=discord.Color.blurple())
         embed.set_author(name=f'{user.name}\'s avatar', icon_url=user.display_avatar.url)
         embed.set_image(url=user.display_avatar.url)
+        await ctx.reply(embed=embed)
     
     @commands.hybrid_command()
     async def banner(self, ctx: commands.Context, user: discord.User = None):
@@ -25,6 +26,7 @@ class Public(commands.Cog):
         embed = discord.Embed(color=discord.Color.blurple())
         embed.set_author(name=f'{user.name}\'s banner', icon_url=user.display_avatar.url)
         embed.set_image(url=user.banner.url if user.banner else None)
+        await ctx.reply(embed=embed)
     
     @commands.hybrid_command()
     async def opt(self, ctx: commands.Context):
@@ -38,7 +40,7 @@ class Public(commands.Cog):
         """
         Measure latency.
         """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
+        await ctx.reply(embed=discord.Embed(description=f'I don\'t see how this will help you, but my ping is `{round(self.bot.latency * 1000)}ms`',color=discord.Color.orange()))
     
     @commands.hybrid_command(aliases=['si'])
     async def serverinfo(self, ctx: commands.Context):
@@ -48,12 +50,19 @@ class Public(commands.Cog):
         raise NotImplementedError('Command requires implementation and permission set-up.')
     
     @commands.hybrid_command(aliases=['ui'])
-    async def userinfo(self, ctx: commands.Context):
+    async def userinfo(self, ctx: commands.Context, member: discord.Member = None):
         """
         Present user information.
         """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
+        member = member or ctx.author
+        embed = discord.Embed(title=f'{member}', color=discord.Color.random())
+        embed.add_field(name='**Server join date**', value=f'{member.joined_at.strftime("%c")}')
+        embed.add_field(name='**Account creation date**', value=f'{member.created_at.strftime("%c")}', inline=False)
+        embed.add_field(name='**Discord ID**', value=f'{member.id}')
+        if member.premium_since:
+            embed.add_field(name='**Nitro boosting since**', value=f'{member.premium_since.strftime("%c")}', inline=False)
+        embed.set_thumbnail(url=member.display_avatar.url)
+        await ctx.reply(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Public(bot))
