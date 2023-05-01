588fedc749fc58a475518a60d6f8067d9eb6f9e9
NathanealV <18462634+NathanealV@users.noreply.github.com>
2023-05-01 18:34:58 +0930

    Added tags to `cached.py`

--- db/cached.py
+++ db/cached.py
@@ -1,18 +1,28 @@
+from typing import Optional
 from async_lru import alru_cache
-from db.models import GuildData, StaffFilter, StaffMonitorMessage, StaffMonitorUser
+from db.models import GuildData, StaffFilter, StaffMonitorMessage, StaffMonitorUser, StaffTag
+
 
 @alru_cache
-async def get_guild_data(guild_id: int) -> GuildData | None:
+async def get_guild_data(guild_id: int) -> Optional[GuildData]:
     return await GuildData.get_or_none(guild_id=guild_id)
+
 
 @alru_cache
 async def get_all_monitor_users() -> list[StaffMonitorUser]:
     return await StaffMonitorUser.all()
 
+
 @alru_cache
 async def get_all_monitor_messages() -> list[StaffMonitorMessage]:
     return await StaffMonitorMessage.all()
 
+
 @alru_cache
 async def get_all_filters() -> list[StaffFilter]:
     return await StaffFilter.all()
+
+
+@alru_cache
+async def get_all_tags() -> list[StaffTag]:
+    return await StaffTag.all()
