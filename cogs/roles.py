e3a67300c8fd4c3bafd0ee15ec72dd3bd449716d
Tharun7605 <44136022+Indiance@users.noreply.github.com>
2023-07-21 18:49:12 +0530

    Formatted roles.py to remove everything barring giverole and cog check

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,63 +1,13 @@
-from typing import Union
-
 import discord
 from discord.ext import commands
+from utils.checks import cogify, admin_command
 
 
 class Roles(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, message: discord.Message):
-        """
-        Increment vote count when reaction is added.
-        """
-        ...
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_delete(self, message: discord.Message):
-        """
-        Decrement vote count when reaction is deleted.
-        """
-        ...
-
-
-    @commands.hybrid_command()
-    async def buttonroles(self, ctx: commands.Context):
-        """
-        Send a message with buttons that add roles.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @commands.hybrid_group()
-    async def cassowary(self, ctx: commands.Context):
-        """
-        Commands for handling cassowaries.
-        """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def create(self, ctx: commands.Context):
-        """
-        Create a cassowary.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def delete(self, ctx: commands.Context):
-        """
-        Delete a cassowary.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def list(self, ctx: commands.Context):
-        """
-        List all cassowaries.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
+    
+    cog_check = cogify(admin_command())
 
     @commands.hybrid_command()
     async def giverole(self, ctx: commands.Context, existing_role: discord.Role, new_role: discord.Role):
@@ -75,29 +25,6 @@ class Roles(commands.Cog):
             except discord.HTTPException:
                 await ctx.send(f"Could not add role to {member.name}")
         await ctx.send("Successfully added roles.")
-
-    @commands.hybrid_group()
-    async def reaction(self, ctx: commands.Context):
-        """
-        Commands for handling reaction roles.
-        """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    
-    @reaction.command(aliases=['add'])
-    async def create(self, ctx: commands.Context):
-        """
-        Create a reaction role.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-    
-    @reaction.command(aliases=['remove'])
-    async def delete(self, ctx: commands.Context):
-        """
-        Delete a reaction role.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Roles(bot))
