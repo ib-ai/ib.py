506f574b9f292d0348978db4643ac9a05e1290d2
Indiance <tharun.tvishy@gmail.com>
2023-07-28 19:43:58 +0530

    Added giverole (#29)
    * Added giverole and swaprole
    
    * Removed the '= None' decorator as well as swaprole command
    
    * Formatted roles.py to remove everything barring giverole and cog check
    
    * Modified code to add logging and format grammatical errors
    
    * Changed logger system & subtracted failures from final number
    
    * Added HTTPException error handling and some more punctuation
    
    * edited error handling and added formatting

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,94 +1,35 @@
-from typing import Union
-
 import discord
 from discord.ext import commands
+from utils.checks import cogify, admin_command
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG) # TODO: change back to logging.info
 
 
 class Roles(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, message: discord.Message):
-        """
-        Increment vote count when reaction is added.
-        """
-        ...
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_delete(self, message: discord.Message):
-        """
-        Decrement vote count when reaction is deleted.
-        """
-        ...
-
+    
+    cog_check = cogify(admin_command())
 
     @commands.hybrid_command()
-    async def buttonroles(self, ctx: commands.Context):
-        """
-        Send a message with buttons that add roles.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @commands.hybrid_group()
-    async def cassowary(self, ctx: commands.Context):
-        """
-        Commands for handling cassowaries.
-        """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def create(self, ctx: commands.Context):
-        """
-        Create a cassowary.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def delete(self, ctx: commands.Context):
-        """
-        Delete a cassowary.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    @cassowary.command()
-    async def list(self, ctx: commands.Context):
-        """
-        List all cassowaries.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-
-    @commands.hybrid_command()
-    async def giverole(self, ctx: commands.Context):
+    async def giverole(self, ctx: commands.Context, existing_role: discord.Role, new_role: discord.Role):
         """
         Assign a new role to all members with a specific role.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-
-    @commands.hybrid_group()
-    async def reaction(self, ctx: commands.Context):
         """
-        Commands for handling reaction roles.
-        """
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
-    
-    @reaction.command(aliases=['add'])
-    async def create(self, ctx: commands.Context):
-        """
-        Create a reaction role.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-    
-    @reaction.command(aliases=['remove'])
-    async def delete(self, ctx: commands.Context):
-        """
-        Delete a reaction role.
-        """ 
-        raise NotImplementedError('Command requires implementation and permission set-up.')
-
+        failure_count = 0	
+        role_members = [member for member in ctx.guild.members if existing_role in member.roles]
+        for member in role_members:
+            try:
+                await member.add_roles(new_role)
+            except discord.Forbidden:
+                return await ctx.send('I do not have permission to add roles to members.')
+            except discord.HTTPException:
+                logger.info(f'Could not add role to {member.name}.')
+                failure_count += 1
+        await ctx.send(f"Successfully added roles to {len(role_members) - failure_count} members.") 
+        logger.info(f'Given {new_role.name} role to {len(role_members) - failure_count} members.')
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Roles(bot))
