b159437499fbd2e03118a587a1a2e80395d8d0db
Tharun7605 <44136022+Indiance@users.noreply.github.com>
2023-07-28 18:58:13 +0530

    Changed logger system & subtracted failures from final number

--- cogs/roles.py
+++ cogs/roles.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from utils.checks import cogify, admin_command
 import logging
 
-logger = logging.getLogger('bot')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG) # TODO: change back to logging.info
 
 
@@ -18,16 +18,16 @@ class Roles(commands.Cog):
         """
         Assign a new role to all members with a specific role.
         """
+        failure_count = 0	
         role_members = [member for member in ctx.guild.members if existing_role in member.roles]
         for member in role_members:
             try:
                 await member.add_roles(new_role)
             except discord.Forbidden:
-                return await ctx.send("I do not have permission to add roles to members.")
-            except discord.HTTPException:
-                await ctx.send(f"Could not add role to {member.name}.")
-        await ctx.send(f"Successfully added roles to {len(role_members)} members.") 
-        logger.info(f'Given {new_role.name} role to {len(role_members)} members.')
+                logger.info(f'Could not add role to {member.name}')
+                failure_count += 1
+        await ctx.send(f"Successfully added roles to {len(role_members) - failure_count} members.") 
+        logger.info(f'Given {new_role.name} role to {len(role_members)-failure_count} members.')
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Roles(bot))
